88a45e9423d653e63fa7fc84b95c4dc96b34bcab
danplo1 <budweiser401@wp.pl>
2017-09-20 19:16:05 +0200

    SpringBootHelloWorldHelloName1

--- src/main/java/danielp/HelloWorldController.java
+++ src/main/java/danielp/HelloWorldController.java
@@ -1,20 +1,21 @@
 package danielp;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorldController {
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @RequestMapping(value = "/hello/world", method = RequestMethod.GET)
     public String helloWorld() {
         return "Hello World!";
     }
 
     @RequestMapping(value = "/hello/{name}", method = RequestMethod.GET)
     public String helloName(@PathVariable(value = "name") String name) {
+        return "Hello" + name;
+    }
+    @RequestMapping(value= "/hello", method = RequestMethod.GET)
+    public String hello (@RequestParam(value = "name") String name){
         return "Hello" + name;
     }
 
