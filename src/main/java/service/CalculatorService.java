0e2d69c8d071f89c9de9f0438eec1d3abe4ba049
danplo1 <budweiser401@wp.pl>
2017-09-22 19:09:33 +0200

    CalcullationController3

--- src/main/java/service/CalculatorService.java
+++ src/main/java/service/CalculatorService.java
@@ -1,9 +1,12 @@
 package service;
 
 
+import org.springframework.stereotype.Service;
 
+import java.math.BigInteger;
 
+@Service
 public interface CalculatorService {
-
+    BigInteger factorial(Long n);
 
 }
